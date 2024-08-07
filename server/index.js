261717f92e852703ed93abeb4e0517e39ae3868f
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2024-08-07 18:01:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
-
-
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import http from "http";
-import mongoose from "mongoose";
-import "dotenv/config";
-import routes from "./src/routes/index.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import http from 'http';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import routes from './src/routes/index.js';
 
 const app = express();
 
@@ -15,29 +13,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Server is running!");
+app.get('/', (req, res) => {
+  res.send('Server is running!');
 });
 
-// Uncomment and add your routes here
-app.use("/api/v3", routes);
+app.use('/api/v3', routes);
 
 const port = process.env.PORT || 5000;
-
-const server = http.createServer(app);
 
 const uri = process.env.MONGODB_URI;
 
 mongoose
-  .connect(uri)
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Mongodb connected");
-    server.listen(port, () => {
+    console.log('Mongodb connected');
+    app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
   })
   .catch((err) => {
-    console.log({ err });
+    console.error('MongoDB connection error:', err);
     process.exit(1);
   });
+
+// Export the app for Vercel
+export default app;
