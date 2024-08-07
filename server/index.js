572fee4d5b75556e22c19a7765200e9ed2db97c7
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2024-08-07 16:56:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,44 +1,3 @@
-// import express from "express";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import http from "http";
-// import mongoose from "mongoose";
-// import "dotenv/config";
-// import routes from "./src/routes/index.js";
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-// // Test route
-// app.get("/", (req, res) => {
-//   res.send("Server is running!");
-// });
-
-// // Uncomment and add your routes here
-// app.use("/api/v3", routes);
-
-// const port = process.env.PORT || 5000;
-
-// const server = http.createServer(app);
-
-// const uri = process.env.MONGODB_URI;
-
-// mongoose
-//   .connect(uri)
-//   .then(() => {
-//     console.log("Mongodb connected");
-//     server.listen(port, () => {
-//       console.log(`Server is listening on port ${port}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log({ err });
-//     process.exit(1);
-//   });
 
 
 import express from "express";
@@ -48,8 +7,6 @@ import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
 import routes from "./src/routes/index.js";
-import serverless from 'serverless-http'; // Import serverless-http package
-
 
 const app = express();
 
@@ -84,7 +41,3 @@ mongoose
     console.log({ err });
     process.exit(1);
   });
-
-
-// Export the handler function for Vercel
-export default serverless(app);
